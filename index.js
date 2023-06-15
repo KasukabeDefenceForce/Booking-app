8d69aa7a4a088691b91450f3f9d1c64a09470929
AbhinavOhri <abhinavohri13@gmail.com>
2023-06-16 02:09:21 +0530

    Installed dependencies.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("connected");
+  } catch (error) {
+    throw error;
+  }
+};
 
 app.listen(8800, () => {
+  connect();
   console.log("Connected to backend");
 });
