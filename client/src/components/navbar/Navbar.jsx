68573242f21c1ab45e771d476c8057b62841cc00
AbhinavOhri <abhinavohri13@gmail.com>
2023-06-17 11:52:52 +0530

    Correcting imports

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import Link from "react-router-dom"
-import "./navbar.css"
+import { Link } from "react-router-dom";
+import "./navbar.css";
+
 const Navbar = () => {
   return (
     <div className="navbar">
