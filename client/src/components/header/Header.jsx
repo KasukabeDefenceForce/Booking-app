db2230d283ac01096f98e299e69d8cbd89d7850f
AbhinavOhri <abhinavohri13@gmail.com>
2023-06-17 12:00:42 +0530

    Added login auth context to header and navbar component

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -13,6 +13,7 @@ import { DateRange } from "react-date-range";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 import { SearchContext } from "../../context/SearchContext";
 import "./header.css";
 
@@ -34,6 +35,8 @@ const Header = ({ type }) => {
   });
 
   const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
+
 
   const handleOption = (name, operation) => {
     setOptions((prev) => {
@@ -88,7 +91,7 @@ const Header = ({ type }) => {
               Get rewarded for your travels – unlock instant savings of 10% or
               more with a free Lamabooking account
             </p>
-            <button className="headerBtn">Sign in / Register</button>
+            {!user && <button className="headerBtn">Sign in / Register</button>}
             <div className="headerSearch">
               <div className="headerSearchItem">
                 <FontAwesomeIcon icon={faBed} className="headerIcon" />
